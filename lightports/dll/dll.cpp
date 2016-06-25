14de4abc367c8a0aedf4a88b088dd33d990ac498
R1tschY <r1tschy@yahoo.de>
2016-06-25 17:52:41 +0200

    outsourced dll class to extra module

---
+++ lightports/dll/dll.cpp
@@ -0,0 +1,34 @@
+#include "dll.h"
+#include "../base/module.h"
+
+namespace Windows {
+
+Dll::Dll() :
+  instance_(nullptr)
+{
+}
+
+Dll& Dll::get()
+{
+  static Dll instance;
+  return instance;
+}
+
+extern "C" BOOL APIENTRY
+DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID) {
+  Dll& self = Dll::get();
+  if (self.instance_ == nullptr) {
+    self.instance_ = hinstDLL;
+  }
+
+  if (fdwReason == DLL_PROCESS_ATTACH) {
+    // for more performance
+    DisableThreadLibraryCalls(hinstDLL);
+
+    Module::setDllInstance(hinstDLL);
+  }
+
+  return true;
+}
+
+} // namespace Windows
---
+++ lightports/dll/dll.h
@@ -0,0 +1,30 @@
+#ifndef DLL_H
+#define DLL_H
+
+#include <windows.h>
+
+namespace Windows {
+
+#define WINDOWS_DLL_SHARED __attribute__((section ("shared"), shared))
+
+extern "C" {
+BOOL APIENTRY DllMain(HINSTANCE, DWORD, LPVOID);
+}
+
+class Dll {
+  Dll();
+public:
+
+  static Dll& get();
+
+  static HINSTANCE getInstance() { return get().instance_; }
+
+private:
+  HINSTANCE instance_;
+
+  friend BOOL APIENTRY DllMain(HINSTANCE, DWORD, LPVOID);
+};
+
+} // namespace Windows
+
+#endif // DLL_H
