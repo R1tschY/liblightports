7eebacb157111e22c1832ca2651ba8696f46c067
R1tschY <r1tschy@yahoo.de>
2016-07-05 17:39:51 +0200

    added TODOs in macros.h

--- lightports/core/macros.h
+++ lightports/core/macros.h
@@ -2,10 +2,12 @@
 #define MACROS_H
 
 // A macro to disallow the copy constructor and operator= functions
+// TODO: remove
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName & operator=(const TypeName&) = delete; \
   TypeName(const TypeName&) = delete
 
+// TODO: move to CPP_MACRO_START/END
 #define WIN_BEGIN_MACRO_BLOCK do {
 #define WIN_END_MACRO_BLOCK } while (false);
 
