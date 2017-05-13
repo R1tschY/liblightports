64f96334106651207f53b595ffde188a698b3c77
R1tschY <r1tschy@yahoo.de>
2017-05-13 20:34:01 +0200

    use WideCharToMultiByte and MultiByteToWideChar

--- lightports/extra/charcodecs.cpp
+++ lightports/extra/charcodecs.cpp
@@ -1,10 +1,6 @@
 #include "charcodecs.h"
 
-#include <stdexcept>
-
 #include <windows.h>
-#include <locale>
-#include <codecvt>
 #include <string>
 
 namespace Windows {
@@ -12,15 +8,47 @@ namespace Windows {
 // convert UTF-8 string to wstring
 std::wstring to_wstring(cpp::string_view str)
 {
-  std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
-  return myconv.from_bytes(str.begin(), str.end());
+  std::wstring result(str.length(), L'\0');
+
+  int chars = ::MultiByteToWideChar(
+      CP_UTF8, 0, str.data(), str.length(),
+      &result[0], result.length() + 1);
+  if (chars == 0)
+  {
+    return std::wstring();
+  }
+  result.resize(chars);
+  return result;
 }
 
 // convert wstring to UTF-8 string
 std::string to_string(cpp::wstring_view str)
 {
-  std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
-  return myconv.to_bytes(str.begin(), str.end());
+  std::string result(str.length(), L'\0');
+
+  int chars = ::WideCharToMultiByte(
+      CP_UTF8, 0, str.data(), str.length(),
+      &result[0], result.length() + 1, nullptr, nullptr);
+  if (chars == 0)
+  {
+    if (::GetLastError() != ERROR_INSUFFICIENT_BUFFER)
+      return std::string();
+
+    chars = ::WideCharToMultiByte(
+        CP_UTF8, 0, str.data(), str.length(),
+        nullptr, 0, nullptr, nullptr);
+    if (chars == 0)
+      return std::string();
+
+    result.resize(chars);
+    chars = ::WideCharToMultiByte(
+          CP_UTF8, 0, str.data(), str.length(),
+          &result[0], result.length() + 1, nullptr, nullptr);
+    if (chars == 0)
+      return std::string();
+  }
+
+  return result;
 }
 
 } // namespace Windows
